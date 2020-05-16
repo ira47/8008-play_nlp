777c628d6eeccfd24b246bf68e01ec08668da0ae
ira47 <1196951630@qq.com>
2020-05-16 09:08:07 +0800

    104 NLTK环境配置与下载器

---
+++ 104-NLTK/01-getting_start.py
@@ -0,0 +1,12 @@
+# https://stackoverflow.com/questions/27750608/error-installing-nltk-supporting-packages-nltk-download
+import nltk
+import ssl
+
+try:
+    _create_unverified_https_context = ssl._create_unverified_context
+except AttributeError:
+    pass
+else:
+    ssl._create_default_https_context = _create_unverified_https_context
+
+nltk.download()
